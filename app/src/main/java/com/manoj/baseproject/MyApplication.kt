8544a8d6d432d7218d45a26f2e7edace2900220e
manojkumar <kumarmanoj@seasiainfotech.com>
2024-08-02 18:35:26 +0530

    initial commit

--- app/src/main/java/com/manoj/baseproject/MyApplication.kt
+++ app/src/main/java/com/manoj/baseproject/MyApplication.kt
@@ -2,6 +2,7 @@ package com.manoj.baseproject
 
 import android.app.Application
 import android.content.Intent
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 
@@ -11,6 +12,7 @@ class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         instance = this
     }
 
