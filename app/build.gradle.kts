d3b3d71667cd4d0fc0c98acd600c35f7acf8ea21
manojkumar <kumarmanoj@seasiainfotech.com>
2024-08-20 11:50:12 +0530

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ plugins {
 
 android {
     namespace = "com.manoj.baseproject"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.manoj.baseproject"
-        minSdk = 28
-        targetSdk = 34
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
